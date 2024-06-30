35a8039a16adf7460ea45d4c77c69b047403e0cf
Martin Dvořák <dvorak.martin.af@gmail.com>
2024-06-30 16:49:16 +0200

    Secondary table final

---
+++ ProjektSQL_secondary.sql
@@ -0,0 +1,22 @@
+CREATE TABLE t_Martin_Dvorak_project_SQL_secondary_final AS
+SELECT 
+	e.country,
+	e.`year` ,
+	e.GDP ,
+	e.population ,
+	e.gini ,
+	c.continent 
+FROM economies e 
+LEFT JOIN countries c ON
+	 e.country =c.country 
+WHERE
+	c.continent LIKE '%europe%'
+	AND e.`year` BETWEEN 
+		(SELECT MIN(payroll_year)
+		 FROM t_martin_dvorak_project_sql_primary_final
+		)
+		AND
+		(SELECT MAX(payroll_year)
+		 FROM t_martin_dvorak_project_sql_primary_final
+		)
+ORDER BY e.country, e.`year` ;

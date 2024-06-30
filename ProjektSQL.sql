8f6fb0a9822501875eb4617f1990ed24b1c5e512
Martin Dvořák <dvorak.martin.af@gmail.com>
2024-06-30 15:26:35 +0200

    Primary table final

--- ProjektSQL.sql
+++ ProjektSQL.sql
@@ -1,48 +1,52 @@
+CREATE TEMPORARY TABLE temp_t_Martin_Dvorak_project_SQL_primary_final AS  -- vytvoření dočasné tabulky, aby bylo možné vytvořit tabulku klasickou včetně CTE WITH
 WITH czechia_price_adjusted AS 
-(
-SELECT date_from,
-	year(date_from) AS year,
-	category_code,
-	value,
-CASE 
-	WHEN MONTH(date_from) BETWEEN 1 AND 3 THEN 1   -- roztřídění záznamů na kvartály, stejně jako v czechia_payroll
-	WHEN MONTH(date_from) BETWEEN 4 AND 6 THEN 2
-	WHEN MONTH(date_from) BETWEEN 7 AND 9 THEN 3
-	WHEN MONTH(date_from) BETWEEN 10 AND 12 THEN 4
-END AS quarter	
-FROM czechia_price cp 
-WHERE region_code IS NULL
-ORDER BY date_from
-)
+	(
+	SELECT date_from,
+		YEAR(date_from) AS year,
+		QUARTER(date_from) AS quarter,
+		category_code,
+		value
+	FROM czechia_price cp 
+	WHERE region_code IS NULL
+	ORDER BY date_from
+	)
 SELECT 
 	cpp.id,
-	cpp.value,
 	cpp.industry_branch_code,
-	cpib.name ,
+	cpib.name AS branch_name,
+	cpp.value AS salary,
 	cpp.payroll_year,
 	cpp.payroll_quarter,
-	czechia_price_adjusted.date_from,
-	czechia_price_adjusted.year,
-	czechia_price_adjusted.quarter,
-	czechia_price_adjusted.category_code,
-	czechia_price_adjusted.value,
-	czechia_price_category.name	
+	cpa.year,
+	cpa.quarter,
+	cpa.category_code,
+	cpa.value AS price,
+	cpc.name,
+	CONCAT(cpp.industry_branch_code ,cpp.payroll_year,cpp.payroll_quarter,cpa.category_code) AS combined_ID
 FROM czechia_payroll cpp
-CROSS JOIN  czechia_price_adjusted
-LEFT JOIN czechia_price_category ON 
-	czechia_price_adjusted.category_code=czechia_price_category.code 
+CROSS JOIN  czechia_price_adjusted cpa
+LEFT JOIN czechia_price_category cpc ON 
+	cpa.category_code=cpc.code 
 LEFT JOIN czechia_payroll_industry_branch cpib ON
 	cpib.code =cpp.industry_branch_code 
 WHERE
 	value_type_code = 5958                				  -- hodnota pro mzdu
-	AND calculation_code =100              				  -- počítáme jen s hodnotou reálnou hodnotou mzdy, nikoliv přepočtenou
+	AND calculation_code =100            				  -- počítáme s fyzickou hodnotou hrubé mzdy
 	AND industry_branch_code IS NOT NULL    			  -- NULL hodnoty v czechia_payroll.industry_branch_code jsou průměry, nepočitám  
-	AND cpp.payroll_year =czechia_price_adjusted.year     -- rok na rok 
-	AND cpp.payroll_quarter=czechia_price_adjusted.quarter-- kvartál na kvartál, zároven vyřadí záznamy, kde nemáme data za korespondující období
+	AND cpp.payroll_year =cpa.year   					  -- rok na rok 
+	AND cpp.payroll_quarter=cpa.quarter					  -- kvartál na kvartál, zároven vyřadí záznamy, kde nemáme data za korespondující období
 ORDER BY industry_branch_code ,
 	cpp.payroll_year ,
 	cpp.payroll_quarter,
-	czechia_price_adjusted.date_from;
+	cpa.date_from,
+	cpa.category_code;
+
+CREATE TABLE t_Martin_Dvorak_project_SQL_primary_final AS   -- Vytvoření finální tabulky
+SELECT * FROM temp_t_Martin_Dvorak_project_SQL_primary_final 
+GROUP BY combined_ID ; 
+
+/* Nyní máme vytvořenou tabulku, kde je každému záznamu průměrné mzdy každého odvětí přiřazen první záznam průměrné ceny za celou ČR v danném kvartálu sledovaných potravin */
 
 
--- just a test for comit validation
+
+

d88e5c41cb7f8be1e8d782bb8ff7ca1d5a751349
Martin Dvořák <dvorak.martin.af@gmail.com>
2024-07-23 09:12:12 +0200

    Clean up formatting errors

--- ProjektSQL_secondary.sql
+++ ProjektSQL_secondary.sql
@@ -1,15 +1,17 @@
 CREATE TABLE t_Martin_Dvorak_project_SQL_secondary_final AS
 SELECT 
 	e.country,
-	e.`year` ,
-	e.GDP ,
-	e.population ,
-	e.gini ,
+	e.`year`,
+	e.GDP,
+	e.population,
+	e.gini,
 	c.continent 
 FROM economies e 
-LEFT JOIN countries c ON
+LEFT JOIN
+	countries c 
+	ON
 	 e.country =c.country 
-WHERE
+	WHERE
 	c.continent LIKE '%europe%'
 	AND e.`year` BETWEEN 
 		(SELECT MIN(payroll_year)
@@ -19,4 +21,4 @@ WHERE
 		(SELECT MAX(payroll_year)
 		 FROM t_martin_dvorak_project_sql_primary_final
 		)
-ORDER BY e.country, e.`year` ;
+ORDER BY e.country, e.`year`;
